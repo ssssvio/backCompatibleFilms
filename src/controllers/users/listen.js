85746e205f730386c7872e25a9b5a44b5589e3fa
ssssvio <r.savio@proton.me>
2024-01-02 21:29:32 -0300

    forma de transferir errors para controller

--- src/controllers/users/listen.js
+++ src/controllers/users/listen.js
@@ -1,19 +1,25 @@
-// const { listenFilms } = require("../../services/users/listenFilms");
-const { schemaListen } = require("../../utils/users/schemaListen");
+const { listenFilms } = require("../../services/users/listenFilms");
 
 async function listen(req, res) {
-
-    try {
-        await schemaListen.validateAsync(req.body);
-        return res.status(200).json({ message: 'ata' })
-    } catch (error) {
-        return res.status(500).json({ message: error.message })
-    }
-    // const { error, data } = await listenFilms(req.body);
-    // return res.status(200).json(data);
-    // if (error) return res.status(error.code).json({
-    //     mensagem: error.message
-    // });
+    const { error, data } = await listenFilms(req.body);
+    if (error) return res.status(error.code).json({
+        mensagem: error.message
+    });
+    return res.status(200).json(data);
 };
 
 module.exports = { listen };
+
+// const { listenFilms } = require("../../services/users/listenFilms");
+
+// async function listen(req, res) {
+//     const { success, data, error, statusCode } = await listenFilms(req.body);
+
+//     return res.status(statusCode).json({
+//         success: success,
+//         data: data,
+//         mensagem: error // Retorna a mensagem de erro do serviço
+//     });
+// }
+
+// module.exports = { listen };
--- src/services/users/listenFilms.js
+++ src/services/users/listenFilms.js
@@ -5,9 +5,9 @@ async function listenFilms(dataFilm) {
     const { name } = dataFilm;
 
     try {
-        console.log(name);
+
         await schemaListen.validateAsync(dataFilm);
-        // const consulta = knexdb('nometabela');
+        const consulta = knexdb('nometabela');
 
     } catch (error) {
         return await (error.message)
@@ -15,3 +15,25 @@ async function listenFilms(dataFilm) {
 }
 
 module.exports = { listenFilms };
+
+// const { knexdb } = require("../../db/conection");
+// const { schemaListen } = require("../../utils/users/schemaListen");
+
+// async function listenFilms(dataFilm) {
+//     const { name } = dataFilm;
+
+//     try {
+//         await schemaListen.validateAsync(dataFilm);
+//         const consulta = knexdb('nometabela');
+
+//         // Aqui você pode adicionar a lógica de processamento dos dados e retornar o resultado
+//         // por exemplo, const result = await consulta...
+
+//         return { success: true, data: result, statusCode: 200 }; // Retornar dados em caso de sucesso
+
+//     } catch (error) {
+//         return { success: false, error: error.message, statusCode: 400 }; // Retornar erro em caso de falha na validação
+//     }
+// }
+
+// module.exports = { listenFilms };
