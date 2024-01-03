69673f89c42fb935655a79e0b4839e167d71600a
ssssvio <r.savio@proton.me>
2024-01-03 10:39:13 -0300

    manipulação de errors pelo service

--- src/controllers/users/listen.js
+++ src/controllers/users/listen.js
@@ -1,25 +1,11 @@
 const { listenFilms } = require("../../services/users/listenFilms");
 
 async function listen(req, res) {
-    const { error, data } = await listenFilms(req.body);
-    if (error) return res.status(error.code).json({
-        mensagem: error.message
-    });
+    const { data, error, statusCode } = await listenFilms(req.body);
+
+    if (error) return res.status(statusCode).json({ mensagem: error });
+
     return res.status(200).json(data);
-};
+}
 
 module.exports = { listen };
-
-// const { listenFilms } = require("../../services/users/listenFilms");
-
-// async function listen(req, res) {
-//     const { success, data, error, statusCode } = await listenFilms(req.body);
-
-//     return res.status(statusCode).json({
-//         success: success,
-//         data: data,
-//         mensagem: error // Retorna a mensagem de erro do serviço
-//     });
-// }
-
-// module.exports = { listen };
--- src/services/users/listenFilms.js
+++ src/services/users/listenFilms.js
@@ -7,33 +7,13 @@ async function listenFilms(dataFilm) {
     try {
 
         await schemaListen.validateAsync(dataFilm);
-        const consult = knexdb('qualquercoisa');
+        // const consult = knexdb('qualquercoisa');
+
+        return { success: true, data: true, statusCode: 200 };
 
     } catch (error) {
-        return await (error.message)
+        return { success: false, error: error.message, statusCode: 400 };
     }
 }
 
 module.exports = { listenFilms };
-
-// const { knexdb } = require("../../db/conection");
-// const { schemaListen } = require("../../utils/users/schemaListen");
-
-// async function listenFilms(dataFilm) {
-//     const { name } = dataFilm;
-
-//     try {
-//         await schemaListen.validateAsync(dataFilm);
-//         const consulta = knexdb('nometabela');
-
-//         // Aqui você pode adicionar a lógica de processamento dos dados e retornar o resultado
-//         // por exemplo, const result = await consulta...
-
-//         return { success: true, data: result, statusCode: 200 }; // Retornar dados em caso de sucesso
-
-//     } catch (error) {
-//         return { success: false, error: error.message, statusCode: 400 }; // Retornar erro em caso de falha na validação
-//     }
-// }
-
-// module.exports = { listenFilms };
