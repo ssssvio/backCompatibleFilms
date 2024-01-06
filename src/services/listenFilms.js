cbafa66801d31f298c910ea24aa3d84c0a7f8c7c
ssssvio <r.savio@proton.me>
2024-01-06 17:20:43 -0300

    repeti código mas vou resolver isso

--- src/services/listenFilms.js
+++ src/services/listenFilms.js
@@ -16,13 +16,25 @@ async function listenFilms(dataFilm) {
             })
             .groupBy('models.name');
 
-        const consult = modelsResult[0].compatiblemodels[0];
-        console.log(typeof consult);
-        // if (modelsResult.compatiblemodels === 1) {
-        //     console.log(true);
-        // }
-
         if (modelsResult.length < 1) return { error: "Película não encontrada!", statusCode: 404 };
+        const consultModels = modelsResult[0].compatiblemodels;
+
+        if (consultModels.length < 2) {
+            const twoConsult = modelsResult[0].modelName;
+
+            const modelsResult2 = await knexdb('models')
+                .select('models.name as modelName')
+                .select(knexdb.raw('ARRAY_AGG(compatiblemodels.compatible_models) as compatibleModels'))
+                .leftJoin('compatiblemodels', 'models.id', '=', 'compatiblemodels.models_id')
+                .where(builder => {
+                    builder.where('models.name', twoConsult)
+                        .orWhere('compatiblemodels.compatible_models', twoConsult);
+                })
+                .groupBy('models.name');
+
+            return { success: true, data: modelsResult2, statusCode: 200 };
+        }
+
         return { success: true, data: modelsResult, statusCode: 200 };
 
     } catch (error) {
