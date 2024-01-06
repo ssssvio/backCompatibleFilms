44526ea3ce4bd14e2a12148de0787fb5380b0535
ssssvio <r.savio@proton.me>
2024-01-06 17:43:21 -0300

    atualizando respostas

--- src/utils/schemaListen.js
+++ src/utils/schemaListen.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 exports.schemaListen = Joi.object({
     name: Joi.string().required().messages({
-        'any.required': 'O campo name é obrigatório!',
-        'string.empty': 'O campo name deve ser preenchido!'
+        'any.required': "The 'name' field is required",
+        'string.empty': "The field 'name' is mandatory!"
     }),
 })
