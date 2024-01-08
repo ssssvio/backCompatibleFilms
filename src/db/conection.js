0d29c8aed9d426ff0722d566e7e7d1f0d42341f4
ssssvio <r.savio@proton.me>
2024-01-08 11:12:55 -0300

    erro de digitação na conexao com db

--- src/db/conection.js
+++ src/db/conection.js
@@ -2,7 +2,7 @@ exports.knexdb = require('knex')({
     client: 'pg',
     connection: {
         host: process.env.DB_HOST,
-        port: process.env.DB_PORTL,
+        port: process.env.DB_PORT,
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_NAME,
