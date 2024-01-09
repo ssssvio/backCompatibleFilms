0c28c8c4e40b7d44035ca546fa7d60c3a90273e4
ssssvio <r.savio@proton.me>
2024-01-09 07:54:42 -0300

    erro resolvido

--- src/services/listenFilms.js
+++ src/services/listenFilms.js
@@ -8,19 +8,20 @@ async function listenFilms(dataFilm) {
 
         await schemaListen.validateAsync(dataFilm);
         const modelsResult = await queryModelsByName(name);
-        if (modelsResult.length < 1) return { error: "Film not found!", statusCode: 404 };
+        if (!modelsResult) return { error: "Film not found!", statusCode: 404 };
 
         const consultModels = modelsResult.compatiblemodels;
         if (consultModels.length < 2) {
             const reModelName = modelsResult.modelName;
             const reModelsResult = await queryModelsByName(reModelName);
-            if (reModelsResult.length < 1) return { error: "Film not found!", statusCode: 404 };
+            if (!reModelsResult) return { error: "Film not found!", statusCode: 404 };
             return { success: true, data: reModelsResult, statusCode: 200 };
         }
 
         return { success: true, data: modelsResult, statusCode: 200 };
 
     } catch (error) {
+        console.log(error);
         const statusCode = error.statusCode || 400;
         return { success: false, error: error.message, statusCode };
     }
