68dbecf2bf612530ab4dc208706e8e9aaa3f1f02
ssssvio <r.savio@proton.me>
2024-01-11 11:10:29 -0300

    schema máximo caracteres.

--- src/utils/schemaListen.js
+++ src/utils/schemaListen.js
@@ -1,8 +1,12 @@
 const Joi = require("joi");
 
 exports.schemaListen = Joi.object({
-    name: Joi.string().required().messages({
-        'any.required': "The 'name' field is required",
-        'string.empty': "The field 'name' is mandatory!"
-    }),
-})
+    name: Joi.string()
+        .max(20)
+        .required()
+        .messages({
+            'any.required': "The 'name' field is required",
+            'string.empty': "The field 'name' is mandatory!",
+            'string.max': "The 'name' field must not exceed 20 characters"
+        }),
+});
