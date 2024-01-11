ce6640f28234cda7ded82e510cc0bb0288ba1bf4
ssssvio <r.savio@proton.me>
2024-01-11 11:00:30 -0300

    n á montanhas que Deus não possa mover.

--- dump.sql
+++ dump.sql
@@ -1,3 +1,16 @@
+-- Reiniciar a sequência para a tabela brand
+SELECT setval('brand_id_seq', 1, false);
+
+-- Reiniciar a sequência para a tabela models
+SELECT setval('models_id_seq', 1, false);
+
+-- Reiniciar a sequência para a tabela compatibleModels
+SELECT setval('compatiblemodels_id_seq', 1, false);
+
+DROP TABLE brand;
+DROP TABLE models;
+DROP TABLE compatibleModels;
+
 CREATE TABLE brand (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL
@@ -16,7 +29,6 @@ CREATE TABLE compatibleModels (
     compatible_models VARCHAR(255) NOT NULL
 );
 
--- input 
 INSERT INTO brand (name) VALUES
     ('SAMSUNG'),
     ('IPHONE'),
@@ -26,34 +38,33 @@ INSERT INTO brand (name) VALUES
     ('REALME')
 
 INSERT INTO models (name, brand_id) VALUES
-1-    ('A12', 1),
-2-    ('7', 2),
-3-    ('7 PLUS', 2),
-4-    ('X', 2),
-5-    ('XR', 2),
-6-    ('XS MAX', 2),
-7-    ('A10S', 1),
-8-    ('A11', 1),
-
-9-    ('13 PRO MAX', 2),
-10-   ('15 PRO', 2),
-11-   ('14 PRO', 2),
-12-   ('NOTE 10', 5),
-13-   ('A01', 1),
-14-   ('NOTE 12 PRO', 5),
-15-   ('Poco C55', 6),
-16-   ('NOTE 12', 5),
-17-   ('NOTE 11', 5),
-18-   ('12', 2),
-19-   ('13', 2),
-20-   ('A52', 1),
-21-   ('G22', 3),
-22-   ('G31', 3),
-23-   ('A50', 1),
-24-   ('12C', 5),
-25-   ('G52', 3),
-26-   ('A20', 1),
-
+    ('A12', 1),
+    ('7', 2),
+    ('7 PLUS', 2),
+    ('X', 2),
+    ('XR', 2),
+    ('XS MAX', 2),
+    ('A10S', 1),
+    ('A11', 1),
+    ('13 PRO MAX', 2),
+    ('15 PRO', 2),
+    ('14 PRO', 2),
+    ('NOTE 10', 5),
+    ('A01', 1),
+    ('NOTE 12 PRO', 5),
+    ('Poco C55', 6),
+    ('NOTE 12', 5),
+    ('NOTE 11', 5),
+    ('12', 2),
+    ('13', 2),
+    ('A52', 1),
+    ('G22', 3),
+    ('G31', 3),
+    ('A50', 1),
+    ('12C', 5),
+    ('G52', 3),
+    ('A20', 1);
+-- proximo n27
 -- deixar so o nome sem a marca antes
 -- deixar a pessoa escrever como quiser, quando passar pelo back end 
 -- passar a req tudo pra maisculo e deixar todo banco de dado em MAIUSCULO
@@ -123,4 +134,4 @@ INSERT INTO compatiblemodels (models_id, compatible_models) VALUES
     (24, '12A'),
     (24, '10C'),
     (25, 'G82'),
-    (26, 'A30'),
+    (26, 'A30');
