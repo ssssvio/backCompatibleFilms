f867b48a9635d06056278849b87eb3cb0615037b
ssssvio <r.savio@proton.me>
2024-01-18 09:48:41 -0300

    add cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const app = express();
 
 const users = require('./routes/users');
 
+app.use(cors());
 app.use(express.json());
 app.use(users.routes);
 
