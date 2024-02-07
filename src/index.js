ecb92a53b6afd1afbe6a74b5670c1f609eb1a9fa
ssssvio <r.savio@proton.me>
2024-02-07 13:34:32 -0300

    podendo acessar!

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const app = express();
 
 const users = require('./routes/users');
 
+app.use(cors({
+    origin: 'https://sincc.netlify.app' // Substitua pelo URL do seu frontend no Netlify
+}));
+
 app.use(cors());
 app.use(express.json());
 app.use(users.routes);
